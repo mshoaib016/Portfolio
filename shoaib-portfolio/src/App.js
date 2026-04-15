623cff816cc7658023a471bca8a2c37175f9ae5b
Muhammad Shoaib <shoaibhanif163@gmail.com>
2026-04-15 11:25:52 +0500

    black screen isssue solve in app.js app.css index.js landingpage.css

--- shoaib-portfolio/src/App.js
+++ shoaib-portfolio/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import LandingPage from "./pages/LandingPage";
 import PortfolioPage from "./pages/PortfolioPage";
@@ -7,19 +7,15 @@ import ResumeViewer from "./pages/ResumeViewer";
 import "./App.css";
 
 function App() {
-  // Localhost ke liye empty, production ke liye basename
-  const basename =
-    process.env.NODE_ENV === "production" ? "/shoaib-portfolio" : "";
-
   return (
     <HelmetProvider>
-      <Router basename={basename}>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/portfolio" element={<PortfolioPage />} />
           <Route path="/resume" element={<ResumeViewer />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </HelmetProvider>
   );
 }
