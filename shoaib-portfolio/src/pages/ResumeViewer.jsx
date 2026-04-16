645dd31ba9be5bc455bfc949a1b71bbe482f065b
Muhammad Shoaib <shoaibhanif163@gmail.com>
2026-04-16 11:28:00 +0500

    add a new folder 404.html changing in index.html, resumeviewer.css and resumeviewer.jsx

--- shoaib-portfolio/src/pages/ResumeViewer.jsx
+++ shoaib-portfolio/src/pages/ResumeViewer.jsx
@@ -1,31 +1,43 @@
-import React from "react";
-import { Helmet } from "react-helmet-async";
-import "./ResumeViewer.css";
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import './ResumeViewer.css';
 
 const ResumeViewer = () => {
   const pdfUrl = "https://mshoaib016.github.io/Portfolio/assets/resume.pdf";
-
+  
   return (
     <>
       <Helmet>
         <title>Resume | Muhammad Shoaib</title>
       </Helmet>
-
+      
       <div className="resume-viewer">
         <div className="toolbar">
           <a href="/Portfolio/" className="back-btn">
             <i className="fas fa-arrow-left"></i> Back to Home
           </a>
-          <a href={pdfUrl} download className="download-btn">
-            <i className="fas fa-download"></i> Download
+          <a href={pdfUrl} target="_blank" rel="noopener noreferrer" className="download-btn">
+            <i className="fas fa-download"></i> Download Resume
           </a>
         </div>
 
-        <iframe
+        {/* Desktop - iframe */}
+        <iframe 
           src={`https://docs.google.com/viewer?url=${pdfUrl}&embedded=true`}
           title="Resume"
-          className="pdf-frame"
+          className="pdf-frame desktop-only"
         />
+
+        {/* Mobile - Direct link */}
+        <div className="mobile-only">
+          <div className="mobile-resume">
+            <i className="fas fa-file-pdf"></i>
+            <p>PDF preview mobile par support nahi hota</p>
+            <a href={pdfUrl} target="_blank" rel="noopener noreferrer" className="view-btn">
+              <i className="fas fa-eye"></i> Resume Kholo
+            </a>
+          </div>
+        </div>
       </div>
     </>
   );
